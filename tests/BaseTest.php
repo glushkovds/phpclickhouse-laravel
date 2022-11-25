b9c2d19540da7d5278294ce82da58ac50b7797c9
Denis Glushkov <amkarovec@gmail.com>
2022-11-25 19:40:35 +0500

    Add more tests

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -8,14 +8,38 @@ use Tests\Models\Example;
 
 class BaseTest extends TestCase
 {
-    public function testMain()
+    public function testWorkWithClient()
     {
-        /** @var \ClickHouseDB\Client $db */
-//        $db = DB::connection('clickhouse')->getClient();
         Artisan::call('migrate');
+        /** @var \ClickHouseDB\Client $db */
+        $db = DB::connection('clickhouse')->getClient();
+        $db->write("TRUNCATE TABLE examples");
+        $db->insert('examples', [[100, 'string']], ['f_int', 'f_string']);
+        $db->write("ALTER TABLE examples UPDATE f_string='updated string' WHERE f_int=100");
+        usleep(1e4);
+        $rows = $db->select("SELECT * FROM examples LIMIT 1")->rows();
+        $this->assertEquals(100, $rows[0]['f_int']);
+        $this->assertEquals('updated string', $rows[0]['f_string']);
+    }
+
+    public function testSimpleModelInsertAndSelect()
+    {
+        Example::truncate();
         Example::insertAssoc([['f_int' => 1, 'f_string' => 'zz']]);
         $rows = Example::select()->getRows();
         $this->assertEquals(1, $rows[0]['f_int']);
         $this->assertEquals('zz', $rows[0]['f_string']);
+    }
+
+    public function testMultipleWheres()
+    {
+        Example::truncate();
+        Example::insertAssoc([['f_int' => 1, 'f_string' => 'zz']]);
+        $query = Example::select()
+            ->whereIn('f_string', ['zz'])
+            ->whereBetween('f_int', [1, 2]);
+        $this->assertEquals("SELECT * FROM `examples` WHERE `f_string` IN ('zz') AND `f_int` BETWEEN 1 AND 2", $query->toSql());
+        $rows = $query->getRows();
+        $this->assertNotEmpty($rows);
     }
 }
