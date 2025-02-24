2f4d0117cd0359ca0852f3293397d76d1aa4dfca
jcsoriano <josecarlosoriano@gmail.com>
2025-02-25 00:04:57 +0800

    Add paginate and simplePaginate

--- src/Builder.php
+++ src/Builder.php
@@ -6,6 +6,9 @@ namespace PhpClickHouseLaravel;
 
 use ClickHouseDB\Client;
 use ClickHouseDB\Statement;
+use Closure;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
 use PhpClickHouseLaravel\Exceptions\QueryException;
 use Tinderbox\ClickhouseBuilder\Query\BaseBuilder;
 
@@ -128,6 +131,75 @@ class Builder extends BaseBuilder
     public function newQuery(): self
     {
         return new static($this->client);
+    }
+
+    /**
+     * Paginate the given query.
+     *
+     * @param  int|null|\Closure  $perPage
+     * @param  array|string  $columns
+     * @param  string  $pageName
+     * @param  int|null  $page
+     * @param  \Closure|int|null  $total
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function paginate($perPage = null, $columns = ['*'], $pageName = 'page', $page = null)
+    {
+        $page = $page ?: Paginator::resolveCurrentPage($pageName);
+
+        $count = (clone $this)->count();
+
+        $perPage = ($perPage instanceof Closure
+            ? $perPage($total)
+            : $perPage
+        ) ?: 15;
+
+        $results = $this->limit($perPage, $perPage * ($page - 1))
+            ->getRows();
+
+        return new LengthAwarePaginator(
+            $results,
+            $count,
+            $perPage,
+            $page,
+            [
+                'path' => Paginator::resolveCurrentPath(),
+                'pageName' => $pageName,
+            ]
+        );
+    }
+
+    /**
+     * Paginate the given query into a simple paginator.
+     *
+     * @param  int|null  $perPage
+     * @param  array|string  $columns
+     * @param  string  $pageName
+     * @param  int|null  $page
+     * @return \Illuminate\Contracts\Pagination\Paginator
+     */
+    public function simplePaginate($perPage = null, $columns = ['*'], $pageName = 'page', $page = null)
+    {
+        $page = $page ?: Paginator::resolveCurrentPage($pageName);
+
+        $perPage = $perPage ?: 15;
+
+        // Next we will set the limit and offset for this query so that when we get the
+        // results we get the proper section of results. Then, we'll create the full
+        // paginator instances for these results with the given page and per page.
+        $this->offset(($page - 1) * $perPage)->limit($perPage + 1);
+
+        return new Paginator(
+            $this->getRows(),
+            $perPage,
+            $page,
+            [
+                'path' => Paginator::resolveCurrentPath(),
+                'pageName' => $pageName,
+            ],
+        );
     }
 
 }
